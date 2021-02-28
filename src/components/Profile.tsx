5536dd0177f7a0be8a1cbdaa613093ec8d6adeee
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2021-02-28 12:39:32 -0300

    chore: using context

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/danrigoni.png" alt="Carlos Daniel Rigoni" />
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Carlos Daniel</strong>
         <p>
           <img src="icons/level.svg" alt="level" />
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
