d89e84b808de8a2f71a712d9e0836a5e3b48b6b9
Carlos Daniel Rigoni <danrigonisocial@gmail.com>
2021-02-27 22:11:15 -0300

    chore: add challenge box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Head from 'next/head';
+
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import styles from '../styles/pages/Home.module.css';
-
-import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -23,7 +24,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
